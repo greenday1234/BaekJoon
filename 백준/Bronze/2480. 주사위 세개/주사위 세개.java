3c6b79c0bee6875c532ad3fa8c914153c90d510e
lee chan hui <89984459+greenday1234@users.noreply.github.com>
2024-02-13 23:49:36 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        int money = 0;
+
+        if (A == B && B == C) {
+            money = 10000 + (A * 1000);
+        }
+        else if (A == B || A == C) {
+            money = 1000 + (A * 100);
+        }
+        else if (B == C) {
+            money = 1000 + (B * 100);
+        }
+        else {
+            int max = Math.max(Math.max(A, B), C);
+            money = max * 100;
+        }
+
+        System.out.println(money);
+
+        scanner.close();
+    }
+}
