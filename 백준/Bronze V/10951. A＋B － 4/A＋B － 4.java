b17241d6da54f64b32aeac9686333779f5281820
lee chan hui <89984459+greenday1234@users.noreply.github.com>
2024-02-15 23:23:50 +0900

    [Bronze V] Title: A+B - 4, Time: 144 ms, Memory: 14604 KB -BaekjoonHub

---
+++ 백준/Bronze V/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write(A + B + "\n");
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+}
